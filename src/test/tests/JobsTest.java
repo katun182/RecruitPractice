31ed55f843b4e0bac1f97cc0f65ee5ecec3699e6
rajnish <rajnish.pradhan@ekbana.net>
2017-11-24 16:50:49 +0545

    files edit

--- src/test/tests/JobsTest.java
+++ src/test/tests/JobsTest.java
@@ -1,23 +1,24 @@
 package tests;
 
-import elements.JobProviders;
+
 import elements.Jobs;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import static tests.BaseTest.driver;
 
 
-public class JobsTest {
+public class JobsTest extends BaseTestLogin {
     private Jobs jobs;
+
     @BeforeMethod
     public void setup(){
         super.setup();
-        jobProviders = new JobProviders(driver);
+        jobs = new Jobs(driver);
+    }
 
 
     @Test
-    public void testThatJobsLinkWoks(){
+        public void testThatJobsLinkWoks(){
         jobs.gotoJobsLink();
     }
 
